20a85b67d20c55e768b15907d1972c35e8fd4362
Stefan Buttigieg <info@stefanbuttigieg.com>
2015-06-26 23:19:25 +0200

    Addition of Chapter 10
    Uploading Code for Chapter 10

---
+++ chapter-10/platforms/ios/www/js/services.js
@@ -0,0 +1,50 @@
+angular.module('ionictodo.services',[]).factory('Todo',['$http','PARSE_CREDENTIALS',function($http,PARSE_CREDENTIALS){
+    return {
+        getAll:function(){
+            return $http.get('https://api.parse.com/1/classes/Todo',{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                }
+            });
+        },
+        get:function(id){
+            return $http.get('https://api.parse.com/1/classes/Todo/'+id,{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                }
+            });
+        },
+        create:function(data){
+            return $http.post('https://api.parse.com/1/classes/Todo',data,{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                    'Content-Type':'application/json'
+                }
+            });
+        },
+        edit:function(id,data){
+            return $http.put('https://api.parse.com/1/classes/Todo/'+id,data,{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                    'Content-Type':'application/json'
+                }
+            });
+        },
+        delete:function(id){
+            return $http.delete('https://api.parse.com/1/classes/Todo/'+id,{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                    'Content-Type':'application/json'
+                }
+            });
+        }
+    }
+}]).value('PARSE_CREDENTIALS',{
+    APP_ID: 'JQngqCLQ3SKk15B8HE8oc701vAXcDNXmf0kyivpf',
+    REST_API_KEY:'8xua1aXwK4fczLEpORgVZIeZCXNprpdLCP9xGVAh'
+});
---
+++ chapter-10/www/js/controllers.js
@@ -0,0 +1,33 @@
+angular.module('ionictodo.controllers',[]).controller('TodoListController',['$scope','Todo',function($scope,Todo){
+
+    Todo.getAll().success(function(data){
+        $scope.items=data.results;
+    });
+
+    $scope.onItemDelete=function(item){
+        Todo.delete(item.objectId);
+        $scope.items.splice($scope.items.indexOf(item),1);
+    }
+
+}]).controller('TodoCreationController',['$scope','Todo','$state',function($scope,Todo,$state){
+
+    $scope.todo={};
+
+    $scope.create=function(){
+        Todo.create({content:$scope.todo.content}).success(function(data){
+            $state.go('todos');
+        });
+    }
+
+
+}]).controller('TodoEditController',['$scope','Todo','$state','$stateParams',function($scope,Todo,$state,$stateParams){
+
+    $scope.todo={id:$stateParams.id,content:$stateParams.content};
+
+    $scope.edit=function(){
+        Todo.edit($scope.todo.id,{content:$scope.todo.content}).success(function(data){
+            $state.go('todos');
+        });
+    }
+
+}]);
